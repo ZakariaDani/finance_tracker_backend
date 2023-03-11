1ea1a804441ed45bac4ab2c78748c4279b780a8d
Zakaria Dani <zakaria.dani2015@gmail.com>
2023-03-11 18:21:03 +0100

    add cors config :ghost:

--- src/main/java/ma/est/finance_tracker/config/CustomConfig.java
+++ src/main/java/ma/est/finance_tracker/config/CustomConfig.java
@@ -1,11 +1,30 @@
 package ma.est.finance_tracker.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CustomConfig {
+
+    @Value("${allowed.origins}")
+    private String allowedOrigin;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(allowedOrigin);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
